d6e021dd15e16656c0d8788785db6f176b3d813e
Red <francois.gayraud@epitech.eu>
2017-11-02 18:34:22 +0100

    refresh firstnabt

--- bimbadaboum/platforms/android/src/main/assets/app/views/home/home.js
+++ bimbadaboum/platforms/android/src/main/assets/app/views/home/home.js
@@ -1,10 +1,14 @@
 var UserViewModel = require("../../shared/view-models/user-view-model");
 var firebase = require("nativescript-plugin-firebase");
 var user = new UserViewModel();
+var view = require("ui/core/view");
+var page;
 
 exports.loaded = function (args) {
+	page = args.object;	
 	firebase.getCurrentUser().then(
     function (result) {
+    	page.bindingContext = { name: result.email };
     },
     function (errorMessage) {
       console.log(errorMessage);
@@ -16,27 +20,48 @@ exports.logout = function () {
 	user.logout();
 }
 
-exports.pushThing = function() {
-	console.log("pushthing");
-	 firebase.push(
-     	 '/users',
-      	{
-        	'first': 'FRANCOIS',
-	        'last': 'GAYRAUD',
-    	    'birthYear': 1994,
-        	'isMale': true,
-        	'address': {
-          	'street': 'foostreet',
-          	'number': 124
-        }
-      }
-  ).then(
-      function (result) {
-        console.log("created key: " + result.key);
-      }
+exports.submitFirstName = function() {
+	//	console.log("mail get = " + userMail);
+	var first = view.getViewById(page, "first");
+	var userId;
+	var userMail;
+	firebase.getCurrentUser().then(
+		function (result) {
+			userMail = result.email;
+			console.log("ici : " + userMail);
+		},
+		function (errorMessage) {
+			console.log(errorMessage);
+		}).then(function () {
+		 var onQueryEvent = function(result) {
+		        if (!result.error) {
+		      	  console.log(JSON.stringify(result.value));
+		      	}
+		    };
+			userId = firebase.query(
+				onQueryEvent,
+	        	"/users",
+    	    		{
+	    	  			singleEvent: true,
+    					orderBy: {
+   						type: firebase.QueryOrderByType.CHILD,
+      	   				value: "mail"
+	    			},
+	    				range: {
+	           			type: firebase.QueryRangeType.EQUAL_TO,
+	           			value: userMail
+	        		},
+	    			})
+		});
+
+	console.log("userId" + userId);
+
+	console.log(first.text);
+	 firebase.update(
+      '/users/-KxxQYJefMwcYKvNUz2a',
+      {'firstName': first.text}
   );
 }
-
 
 exports.queryThing = function() {
 
@@ -54,11 +79,11 @@ exports.queryThing = function() {
       	singleEvent: true,
     	orderBy: {
    		   type: firebase.QueryOrderByType.CHILD,
-      	   value: 'birthYear'
+      	   value: "mail"
     	},
     	range: {
            type: firebase.QueryRangeType.EQUAL_TO,
-           value: 1994
+           value: "jetest@gmail.com"
         },
     });
   
